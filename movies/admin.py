ed820f0d3a4fe460942c9918f329dbe109c4a462
Dazzy132 <vladsolo23rus@gmail.com>
2022-04-08 19:26:00 +0300

    Урок 6

--- movies/admin.py
+++ movies/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+
+class GenreAdmin(admin.ModelAdmin):
+    prepopulated_fields = {"url": ("name",)}
+
+class MovieAdmin(admin.ModelAdmin):
+    prepopulated_fields = {"url": ("title",)}
+
+admin.site.register(Category)
+admin.site.register(Genre, GenreAdmin)
+admin.site.register(Movie, MovieAdmin)
+admin.site.register(MovieShots)
+admin.site.register(Actor)
+admin.site.register(Rating)
+admin.site.register(RatingStar)
+admin.site.register(Reviews)
