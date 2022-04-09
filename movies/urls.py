848aea065d0ef1c5c3f87ba9975ed4eef6d928fc
Dazzy132 <vladsolo23rus@gmail.com>
2022-04-09 19:57:01 +0300

    Урок 17. Создание фильтрации по фильмам/жанрам

--- movies/urls.py
+++ movies/urls.py
@@ -3,6 +3,8 @@ from .views import *
 
 urlpatterns = [
     path('', MoviesView.as_view(), name='home'),
+    path('filter/', FilterMoviesView.as_view(), name='filter'),
+    # filter стоит выше чтобы данный url не попадал в поле поиска по слагу
     path("<slug:slug>/", MovieDetailView.as_view(), name='movie_detail'),
     # Первый аргумент <slug: - то, что мы запрашиваем в models.py (get_absolute_url). Второй аргумент, то что мы передаем в views.py
     path('review/<int:pk>/', AddReview.as_view(), name="add_review"),
--- movies/views.py
+++ movies/views.py
@@ -1,3 +1,4 @@
+from django.db.models import Q
 from django.shortcuts import render, redirect
 from django.views import View
 from django.views.generic import ListView, DetailView
@@ -5,7 +6,20 @@ from django.views.generic import ListView, DetailView
 from .models import *
 from .forms import ReviewsForm
 
-class MoviesView(ListView):
+
+# Похож на метод get_context_data(). Передает свои данные в другие классы
+class GenreYear:
+    """Жанры и года выхода фильмов"""
+    def get_genres(self):
+        return Genre.objects.all()
+    # Получение всех жанров
+
+    def get_years(self):
+        return Movie.objects.filter(draft=False).values("year")
+    # Получение всех фильмов которые не черновики, забираем только поле с годом ( values("year") )
+
+
+class MoviesView(GenreYear, ListView):
     """Список фильмов"""
     model = Movie
     queryset = Movie.objects.filter(draft=False)
@@ -13,7 +27,8 @@ class MoviesView(ListView):
     # template_name можно не указывать если писать шаблоны (Название модели_list) или detail. Зависит от класса
     # context_object_name не указывается, потому что обращение в шаблонах идет непосредственно к имени модели - movie.
 
-class MovieDetailView(DetailView):
+
+class MovieDetailView(GenreYear, DetailView):
     """Полное описание фильма"""
     model = Movie
     slug_field = "url"
@@ -48,10 +63,51 @@ class AddReview(View):
         return redirect(movie.get_absolute_url())
 
 
-class ActorView(DetailView):
+class ActorView(GenreYear, DetailView):
     """Вывод информации об актере"""
     model = Actor
     template_name = 'movies/actor.html'
     slug_field = 'name'
     # Поле по которому искать актеров - slug_field
+
+
+class FilterMoviesView(GenreYear, ListView):
+    """Фильтрация фильмов"""
+
+    def get_queryset(self):
+        queryset = Movie.objects.all()
+        # Назначаем переменной модель Movie
+        if "year" in self.request.GET:
+            # Если год есть в запросе
+            queryset = queryset.filter(year__in=self.request.GET.getlist("year"))
+            # queryset = фильтруем по запрошенному году/годам поле 'year'
+        if "genre" in self.request.GET:
+            # Если жанр есть в запросе
+            queryset = queryset.filter(genres__in=self.request.GET.getlist("genre"))
+            # queryset = фильтруем по запрошенному жанр/жанрам поле 'genre'
+        return queryset
+
+
+
+
+
+    # def get_queryset(self):
+    #     queryset = Movie.objects.filter(
+    #         Q(year__in=self.request.GET.getlist('year')) |
+    #         Q(genres__in=self.request.GET.getlist('genre'))
+    #     )
+    #     # C помощью метода Q можно будет запрашивать или года или жанры. (ИЛИ - | )
+    #     return queryset
+
+
+        # queryset = Movie.objects.filter(
+        #     year__in=self.request.GET.getlist('year'), genres__in=self.request.GET.getlist('genre')
+        # )
+        # Чтобы совпадал и год и жанр
+
+
+
+# Фильтруем фильмы, где года будут входить в список, который будет возвращаться с фронтенда.
+# Фильтрация равна запросу, с помощью метода GET, достается с помощью getlist(поле которое получаем)
+# Чтобы применить фильтрацию, нужно будет обернуть поле в форму и вызвать метод get
 
